c1ba3e151dab0148b3d445a906d3c95937cf3624
Robert Kirui <kirugik79@gmail.com>
2022-05-26 17:41:06 +0300

    deploy to heroku

---
+++ news/tests.py
@@ -0,0 +1,93 @@
+from django.test import TestCase
+from .models import Editor,Article,Tags
+import datetime as dt 
+
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.robert = Editor(first_name = 'Robert', last_name ='Kirui', email ='robert.kirui@student.moringaschool.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.robert, Editor))  
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.robert.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0) 
+
+
+
+
+
+class TagsTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.new_tag = Tags(name = 'Flatiron')
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_tag, Tags))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.new_tag.save_tag()
+        tags = Tags.objects.all()
+        self.assertTrue(len(tags) > 0)
+
+
+
+
+
+# class ArticleTestClass(TestCase):
+#     # Set up method
+#     def setUp(self):
+#         self.new_article = Article(title = 'Moringa partnership with Flatiron School', post = 'Moringa has licensed the Flatiron School curriculum and learning platform, and will deliver the material independently', pub_date = '2022-02-17') 
+
+#     # Testing  instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_article, Article))
+    
+#     # Testing Save Method
+#     def test_save_method(self):
+#         self.new_article.save_article()
+#         articles = Article.objects.all()
+#         self.assertTrue(len(articles) > 0) 
+
+
+
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.robert = Editor(first_name = 'Robert', last_name = 'Kirui', email = 'robert.kirui@student.moringaschool.com')
+        self.robert.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = Tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article = Article(title = 'Test Article', post = 'This is a random test Post', editor = self.robert)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        Tags.objects.all().delete()
+        Article.objects.all().delete()
+
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0) 
+    
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0) 
